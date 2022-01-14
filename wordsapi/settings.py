db7d43510905d020f7c331b582ff717a1c9ed02c
Carla Aluvai <carngie@gmail.com>
2022-01-14 17:51:52 +0300

    heroku errors

--- wordsapi/settings.py
+++ wordsapi/settings.py
@@ -11,12 +11,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+
+import django_heroku
 import environ
+import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()
 
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv('DEBUG'))
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -141,3 +143,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+django_heroku.settings(locals())
