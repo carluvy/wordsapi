aa0bfd6f30cf4906aa9dac140dc68bdfc7a4230c
Carla Aluvai <aluvaicarla@gmail.com>
2022-01-19 13:08:32 +0300

    removed pagination

--- wordsapi/settings.py
+++ wordsapi/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10
 }
 # django_heroku.settings(locals())
